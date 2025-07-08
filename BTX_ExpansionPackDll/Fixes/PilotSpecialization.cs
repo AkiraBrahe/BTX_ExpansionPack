3f0e3c318d739b20cdaeaaddd8fef3a6dabf5bc7
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-07-08 13:25:12 +0200

    Prevent pilots without specialization

--- BTX_ExpansionPackDll/Fixes/PilotSpecialization.cs
+++ BTX_ExpansionPackDll/Fixes/PilotSpecialization.cs
@@ -1,10 +1,15 @@
 ﻿using BattleTech;
+using BattleTech.UI;
 using System.Collections.Generic;
 
 namespace BTX_ExpansionPack.Fixes
 {
     internal class PilotSpecialization
     {
+        private const string MechPilotTag = "pilot_mech_pilot";
+        private const string VehiclePilotTag = "pilot_vehicle_crew";
+        private const string NoMechPilotTag = "pilot_nomech_crew";
+
         [HarmonyPatch(typeof(PilotGenerator), "GeneratePilots")]
         public static class PilotGenerator_GeneratePilots
         {
@@ -13,23 +18,41 @@ namespace BTX_ExpansionPack.Fixes
             {
                 foreach (PilotDef pilot in __result)
                 {
-                    if (pilot.PilotTags.Contains("pilot_nomech_crew") && !pilot.PilotTags.Contains("pilot_vehicle_crew"))
+                    if (pilot.PilotTags.Contains(NoMechPilotTag) && !pilot.PilotTags.Contains(VehiclePilotTag))
                     {
-                        pilot.PilotTags.Add("pilot_vehicle_crew");
-                        pilot.PilotTags.Remove("pilot_mech_pilot");
+                        pilot.PilotTags.Add(VehiclePilotTag);
+                        pilot.PilotTags.Remove(MechPilotTag);
                     }
-                    else if (!pilot.PilotTags.Contains("pilot_mech_pilot") && !pilot.PilotTags.Contains("pilot_vehicle_crew"))
+                    else if (!pilot.PilotTags.Contains(MechPilotTag) && !pilot.PilotTags.Contains(VehiclePilotTag))
                     {
                         if (UnityEngine.Random.Range(0, 5) == 0)
                         {
-                            pilot.PilotTags.Add("pilot_mech_pilot");
+                            pilot.PilotTags.Add(MechPilotTag);
                         }
                         else
                         {
-                            pilot.PilotTags.Add("pilot_vehicle_crew");
+                            pilot.PilotTags.Add(VehiclePilotTag);
                         }
                     }
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(SGBarracksMWDetailPanel), "DisplayPilot")]
+        public static class SGBarracksMWDetailPanel_DisplayPilot
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Pilot pilot)
+            {
+                if (pilot == null || pilot.pilotDef == null || pilot.pilotDef.PilotTags == null)
+                    return;
+
+                bool hasMechSpecialization = pilot.pilotDef.PilotTags.Contains(MechPilotTag);
+                bool hasVehicleSpecialization = pilot.pilotDef.PilotTags.Contains(VehiclePilotTag);
+
+                if (hasMechSpecialization || hasVehicleSpecialization) return;
+                Main.Log.LogDebug($"Pilot '{pilot.Callsign}' has no specialization. Assigning one.");
+                pilot.pilotDef.PilotTags.Add(MechPilotTag);
             }
         }
     }
