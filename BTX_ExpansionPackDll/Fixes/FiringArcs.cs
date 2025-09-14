a080af4b694087ecaed9e0744d812a524f31dfd5
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-09-14 23:42:02 +0200

    Refactor vehicle firing arc logic

--- BTX_ExpansionPackDll/Fixes/FiringArcs.cs
+++ BTX_ExpansionPackDll/Fixes/FiringArcs.cs
@@ -7,11 +7,11 @@ namespace BTX_ExpansionPack.Fixes
     internal class FiringArcs
     {
         [HarmonyPatch(typeof(Mech), "IsTargetPositionInFiringArc")]
-        public static class FiringArcOverrideLogic
+        public static class Mech_IsTargetPositionInFiringArc
         {
             [HarmonyPrefix]
             [HarmonyWrapSafe]
-            [HarmonyBefore("io.mission.customunits")]
+            [HarmonyPriority(Priority.First)]
             public static bool Prefix(ref bool __runOriginal, Mech __instance, ICombatant targetUnit, Vector3 attackPosition, Quaternion attackRotation, Vector3 targetPosition, ref bool __result)
             {
                 // Special case: Standing on building
@@ -40,37 +40,69 @@ namespace BTX_ExpansionPack.Fixes
                 float currentAngle = Mathf.Abs(Mathf.DeltaAngle(
                     PathingUtil.GetAngle(attackRotation * Vector3.forward),
                     PathingUtil.GetAngle(targetPosition - attackPosition)));
-
                 __result = currentAngle < firingArc;
                 __runOriginal = false;
                 return false;
             }
+        }
 
-            private static float GetFiringArc(Mech mech, ICombatant targetUnit, Vector3 attackPosition)
+        [HarmonyPatch(typeof(Vehicle), "IsTargetPositionInFiringArc")]
+        public static class Vehicle_IsTargetPositionInFiringArc
+        {
+            [HarmonyPrefix]
+            [HarmonyWrapSafe]
+            [HarmonyPriority(Priority.First)]
+            public static bool Prefix(ref bool __runOriginal, Vehicle __instance, ICombatant targetUnit, Vector3 attackPosition, Quaternion attackRotation, Vector3 targetPosition, ref bool __result)
             {
-                float firingArc = mech.Combat.Constants.ToHit.FiringArcDegrees;
-
-                if (mech is FakeVehicleMech)
-                {
-                    var vehicleDef = mech.MechDef?.toVehicleDef(mech.MechDef.DataManager);
-                    return vehicleDef?.Chassis != null ? vehicleDef.Chassis.HasTurret ? 360f : 90f : 90f;
-                }
-
-                if (mech is (Mech or QuadMech) and not TrooperSquad)
-                {
-                    float distance = Vector3.Distance(attackPosition, targetUnit.CurrentPosition);
-                    if (distance < Core.Settings.CloseRangeFiringArcDistance)
-                        return Core.Settings.CloseRangeFiringArc;
-
-                    var tags = mech.MechDef?.Chassis?.ChassisTags;
-                    if (tags?.Contains("mech_quirk_notorsotwist") == true)
-                        return firingArc / 2f;
-                    if (tags?.Contains("mech_quirk_extendedtorsotwist") == true)
-                        return firingArc * 2f;
-                }
-
-                return firingArc;
+                float firingArc = GetFiringArc(__instance, targetUnit, attackPosition);
+                float currentAngle = Mathf.Abs(Mathf.DeltaAngle(
+                    PathingUtil.GetAngle(attackRotation * Vector3.forward),
+                    PathingUtil.GetAngle(targetPosition - attackPosition)));
+                __result = currentAngle < firingArc;
+                __runOriginal = false;
+                return false;
             }
+        }
+
+        private static float GetFiringArc(AbstractActor actor, ICombatant targetUnit, Vector3 attackPosition)
+        {
+            if (actor is Vehicle vehicle)
+            {
+                return vehicle.VehicleDef.Chassis.HasTurret ? 360f : 90f;
+            }
+
+            if (actor is Mech mech)
+            {
+                if (mech is FakeVehicleMech fakeVehicle)
+                {
+                    var vehicleDef = fakeVehicle.MechDef?.toVehicleDef(fakeVehicle.MechDef.DataManager);
+                    return vehicleDef?.Chassis != null && vehicleDef.Chassis.HasTurret ? 360f : 90f;
+                }
+
+                return GetMechFiringArc(mech, targetUnit, attackPosition);
+            }
+
+            return actor.Combat.Constants.ToHit.FiringArcDegrees;
+        }
+
+        private static float GetMechFiringArc(Mech mech, ICombatant targetUnit, Vector3 attackPosition)
+        {
+            float firingArc = mech.Combat.Constants.ToHit.FiringArcDegrees;
+
+            if (mech is (Mech or QuadMech) and not TrooperSquad)
+            {
+                float distance = Vector3.Distance(attackPosition, targetUnit.CurrentPosition);
+                if (distance < Core.Settings.CloseRangeFiringArcDistance)
+                    return Core.Settings.CloseRangeFiringArc;
+
+                var tags = mech.MechDef?.Chassis?.ChassisTags;
+                if (tags?.Contains("mech_quirk_notorsotwist") == true)
+                    return firingArc / 2f;
+                if (tags?.Contains("mech_quirk_extendedtorsotwist") == true)
+                    return firingArc * 2f;
+            }
+
+            return firingArc;
         }
     }
 }
