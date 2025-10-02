58077486d0f005813ef8f1233f5cc7b794d16f0d
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-10-02 13:39:06 +0200

    Restrict AMS modes on rapid-fire autocannons

--- BTX_ExpansionPackDll/Fixes/Targeting/RapidFireTargeting.cs
+++ BTX_ExpansionPackDll/Fixes/Targeting/RapidFireTargeting.cs
@@ -1,15 +1,42 @@
 ﻿using BattleTech;
 using BTX_ExpansionPack.Helpers;
 using CustAmmoCategories;
+using Quirks.Quirks.MechEffects;
 using System.Linq;
 
 namespace BTX_ExpansionPack.Fixes.Targeting
 {
-    /// <summary>
-    /// Handles AI targeting logic for rapid fire autocannons based on missile threat.
-    /// </summary>
-    internal class RapidFireTargeting
+
+    public static class RapidFireTargeting
     {
+        /// <summary>
+        /// Restricts AMS modes on Rapid-Fire Autocannons to mechs with Anti-Aircraft Targeting.
+        /// </summary>
+        [HarmonyPatch(typeof(Mech), "InitStats")]
+        public static class Mech_InitStats
+        {
+            [HarmonyPostfix]
+            [HarmonyAfter("BEX.BattleTech.Extended_CE")]
+            public static void Postfix(Mech __instance)
+            {
+                bool hasAATargeting = MechQuirkInfo.MechQuirkStore.TryGetValue(__instance.MechDef.Chassis.Description.Id, out var quirk) && quirk.AntiAircraftTargeting ||
+                    (__instance.FakeVehicle() && __instance.MechDef.MechTags.Contains("unit_vehicle_airDefense"));
+                if (hasAATargeting) return;
+
+                foreach (var weapon in __instance.Weapons)
+                {
+                    if (weapon.IsRFAC())
+                    {
+                        weapon.info().DisableMode("AMS");
+                        weapon.info().DisableMode("AMS_Plus");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles AI targeting logic for rapid fire autocannons based on missile threat.
+        /// </summary>
         [HarmonyPatch(typeof(AttackEvaluator), "MakeAttackOrder")]
         public static class AttackEvaluator_MakeAttackOrder
         {
@@ -22,28 +49,33 @@ namespace BTX_ExpansionPack.Fixes.Targeting
                     return;
                 }
 
-                var rapidFireWeapons = attackOrder.Weapons.Where(w => w.weaponDef.ComponentTags.Contains("RapidFireAutocannon"));
+                var rapidFireWeapons = attackOrder.Weapons.Where(w => w.IsRFAC());
                 if (!rapidFireWeapons.Any())
                     return;
 
                 bool isMissileThreatened = unit.IsMissileThreatened();
                 foreach (Weapon weapon in rapidFireWeapons)
                 {
-                    if (weapon.weaponDef.ComponentTags.Contains("RapidFireAutocannon"))
+                    if (isMissileThreatened) // && weapon.info().isModeAvailble(weapon.info().modes["AMS"], out _))
                     {
-                        if (isMissileThreatened)
-                        {
-                            weapon.setCantAMSFire(false);
-                            weapon.forceMode("AMS");
-                        }
-                        else
-                        {
-                            weapon.setCantNormalFire(false);
-                            weapon.forceMode("RF");
-                        }
+                        weapon.setCantAMSFire(false);
+                        weapon.forceMode("AMS");
+                    }
+                    else
+                    {
+                        weapon.setCantNormalFire(false);
+                        weapon.forceMode("RF");
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Determines if a weapon is a rapid-fire autocannon.
+        /// </summary>
+        public static bool IsRFAC(this Weapon weapon)
+        {
+            return weapon.info()?.modes.ContainsKey("RF") ?? false;
         }
     }
 }
