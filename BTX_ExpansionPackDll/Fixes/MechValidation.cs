c615e3b8459ce890f4fac2e52602e5fa3f17b9f7
Kierk <136152906+AkiraBrahe@users.noreply.github.com>
2025-10-29 05:32:29 +0100

    Count melee weapons as valid weapons

---
+++ BTX_ExpansionPackDll/Fixes/MechValidation.cs
@@ -0,0 +1,52 @@
+﻿using BattleTech;
+using BattleTech.Data;
+using Localize;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BTX_ExpansionPack.Fixes
+{
+    internal class MechValidation
+    {
+        /// <summary>
+        /// Ensures that melee weapons count as valid weapons when validating mech loadouts.
+        /// </summary>
+        [HarmonyPatch(typeof(MechValidationRules), "ValidateMechPosessesWeapons")]
+        public static class MechValidationRules_ValidateMechPosessesWeapons
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(MechDef mechDef, MechValidationLevel validationLevel, WorkOrderEntry_MechLab baseWorkOrder, ref Dictionary<MechValidationType, List<Text>> errorMessages)
+            {
+                bool hasFunctionalWeapon = false;
+                for (int i = 0; i < mechDef.Inventory.Length; i++)
+                {
+                    var mechComponentRef = mechDef.Inventory[i];
+                    if (mechComponentRef.DamageLevel == ComponentDamageLevel.Functional ||
+                        mechComponentRef.DamageLevel == ComponentDamageLevel.NonFunctional ||
+                        MechValidationRules.MechComponentUnderMaintenance(mechComponentRef, validationLevel, baseWorkOrder))
+                    {
+                        if (mechComponentRef.ComponentDefType == ComponentType.Weapon)
+                        {
+                            hasFunctionalWeapon = true;
+                            break;
+                        }
+                        else if (mechComponentRef.ComponentDefType == ComponentType.Upgrade &&
+                                 mechComponentRef.Def.ComponentTags.Any(tag => tag == "MeleeWeapon"))
+                        {
+                            hasFunctionalWeapon = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!hasFunctionalWeapon)
+                {
+                    MechValidationRules.AddErrorMessage(ref errorMessages, MechValidationType.WeaponsMissing, new Text("MISSING WEAPONS: This 'Mech must mount at least one functional Weapon", []));
+                }
+
+                return false;
+            }
+        }
+    }
+}
